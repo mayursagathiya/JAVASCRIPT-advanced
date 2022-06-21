c78c2315e71b55765e5801d079d4684b10cbbe33
mayursagathiya98 <mayursagathiya98@gmail.com>
2022-06-21 17:22:38 +0530

    datatypes

---
+++ tutorials/data_type.js
@@ -0,0 +1,79 @@
+//this file contains basic js datatypes 
+
+// var a=1;
+// console.log(typeof a)
+// num
+
+// var a="1";
+// console.log(typeof a)
+//str
+
+// var a=[]
+// console.log(typeof a)
+// obj
+
+// var a={}
+// console.log(typeof a)
+// obj
+
+
+//addtion 
+
+// const a=1
+// const b="1"
+// console.log(a+b)
+//11
+
+
+// const a=1
+// const b=1
+// console.log(a+b)
+//2
+
+
+// console.log(1+1)
+// 2
+
+// console.log("1"+"1")
+// 11
+
+
+
+// let a
+// console.log(a)
+// undefined
+
+// let a = "He is called 'bb'"; 
+// console.log(a)
+// He is called 'bb'
+
+// let y = 123e5; 
+// console.log(y)
+
+// let x = 5;
+// let y = 5;
+// let z = 6;
+// console.log(x == y)    //true   
+// (x == z)  false
+
+
+// const vars = ["a", "b", "c"];
+// console.log(vars)
+// //[ 'a', 'b', 'c' ]
+// console.log(typeof vars)
+// //object
+
+
+
+// const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// // console.log(person)
+// console.log(typeof person)
+
+
+// let a
+// console.log(typeof a)
+//undefined
+
+// let a = undefined
+// console.log(a)//undefind
+// console.log(typeof a);//undefind
