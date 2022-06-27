0e6c176550741c67adde144b2c40cc8229fe1801
mayursagathiya98 <mayursagathiya98@gmail.com>
2022-06-27 22:45:09 +0530

    scope and op

---
+++ tutorials/operators.js
@@ -0,0 +1,29 @@
+// arithmetic operator
+
+//+ - * / %
+
+// ** //ghat // means 2 ni 4 ghat ===>2^4
+// console.log(2**4) 
+// 16
+//================================
+// ++  
+    // let a=3
+    // a++
+    // console.log(a)
+    //4
+//================================
+//-- 
+    // let a=3
+    // a--
+    // console.log(a)
+//2
+//================================
+// - unary
+    // let a=3
+    // console.log(-a)
+// -3
+//================================
+// + unary 
+// let a=+"3" //conole.log(+a) // +ctx.req.body.phone
+// console.log(typeof a)
+//================================
---
+++ tutorials/variable_scope.js
@@ -0,0 +1,21 @@
+// in this tut we will learn about js variable scope
+// kayo var kya use kari sakiye 
+// const and let  banne blk scp chhe
+
+// jo globally variable hase to blk ni andar use thay 
+// but jo blk ni andar hoy to ae blk ni bar use na thay bcs blk scp che
+
+{
+const a=3
+{
+    // let a=2
+    // console.log(a)
+}console.log(a)
+}
+// console.log(a)
+// ReferenceError: a is not defined
+
+
+
+
+
