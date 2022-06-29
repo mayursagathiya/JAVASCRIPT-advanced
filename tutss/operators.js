9d99c9570eefb32e187f4010d51f225cdca7f0e4
mayursagathiya98 <mayursagathiya98@gmail.com>
2022-06-29 10:01:01 +0530

    op

--- tutss/operators.js
+++ tutss/operators.js
@@ -27,3 +27,73 @@
 // let a=+"3" //conole.log(+a) // +ctx.req.body.phone
 // console.log(typeof a)
 //================================
+
+
+
+
+//=============================
+// comparison opertator
+//  let x=5
+// ==	equal to	x == 8	false	
+// x == 5	true	
+// x == "5"	true	
+// ===	equal value and equal type	x === 5	true	
+// x === "5"	false	
+// !=	not equal	x != 8	true	
+// !==	not equal value or not equal type	x !== 5	false	
+// x !== "5"	true	
+// x !== 8	true	
+// >	greater than	x > 8	false	
+// <	less than	x < 8	true	
+// >=	greater than or equal to	x >= 8	false	
+// <=	less than or equal to	x <= 8	true
+
+
+//===================================
+//relational or logical
+// x = 6 and y = 3
+// &&	and	(x < 10 && y > 1) is true	
+// ||	or	(x == 5 || y == 5) is false	
+// !	not	!(x == y) is true
+
+// or ma koi pan ek condition satify thay to ae andar ghusi jas
+// let  x = 6 
+// let y = 3
+// if	( x == 5 || x == 6 )
+// {
+//     console.log("in")
+// }
+// else{
+//     console.log("else")
+// }
+
+//ternary
+// console.log(x==5 || x==6? true:false)
+
+
+
+
+///////////////========/////////////////////////////////////////////////////////////to do
+//assignment operator
+// Operator	Example	Same As
+// =	x = y	x = y
+// +=	x += y	x = x + y
+// -=	x -= y	x = x - y
+// *=	x *= y	x = x * y
+// /=	x /= y	x = x / y
+// %=	x %= y	x = x % y
+// <<=	x <<= y	x = x << y
+// >>=	x >>= y	x = x >> y
+// >>>=	x >>>= y	x = x >>> y
+// &=	x &= y	x = x & y
+// ^=	x ^= y	x = x ^ y
+// |=	x |= y	x = x | y
+// **=	x **= y	x = x ** y
+
+
+// let  x = 6 
+// let y = 3
+// const g=	x = x & y//x &= y
+// console.log(g)
+
+//===========================================
